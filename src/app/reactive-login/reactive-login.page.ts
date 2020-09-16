5b7df135437600d0fba79be0b5d101df6c95ee97
daftclaud <cdsmileyone@gmail.com>
2020-09-16 07:13:22 -0400

    reverted to starting point

--- src/app/reactive-login/reactive-login.page.ts
+++ src/app/reactive-login/reactive-login.page.ts
@@ -7,11 +7,8 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./reactive-login.page.scss'],
 })
 export class ReactiveLoginPage implements OnInit {
-  email = new FormControl('', Validators.required);
-  password = new FormControl('', [
-    Validators.required,
-    Validators.minLength(6),
-  ]);
+  email;
+  password;
 
   constructor() {}
 
